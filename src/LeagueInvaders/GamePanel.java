c28b8d63bcf8ef6b9344afb2e11f3b3a5acda485
AnkitBisain <league@iMac.attlocal.net>
2018-03-19 18:33:53 -0700

    Continued LEagueInvaders

--- src/LeagueInvaders/GamePanel.java
+++ src/LeagueInvaders/GamePanel.java
@@ -1,5 +1,50 @@
 package LeagueInvaders;
 
-public class GamePanel {
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
+	Timer timer;
+	GameObject gameObject;
+
+	public GamePanel() {
+		timer = new Timer(1000 / 60, this);
+		gameObject = new GameObject(10,10,100,100);
+	}
+
+	void startGame() {
+		timer.start();
+
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		gameObject.draw(g);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		gameObject.update();
+		repaint();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+	}
 }
--- src/LeagueInvaders/LeagueInvaders.java
+++ src/LeagueInvaders/LeagueInvaders.java
@@ -1,25 +1,32 @@
 package LeagueInvaders;
 
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 
 public class LeagueInvaders {
-	
+
 	JFrame frame;
+	GamePanel panel;
 	final int width = 500;
 	final int height = 800;
 
 	public LeagueInvaders() {
+		panel = new GamePanel();
 		frame = new JFrame();
 		setup();
 	}
-	
+
 	void setup() {
+		frame.addKeyListener(panel);
+		frame.add(panel);
 		frame.pack();
 		frame.setSize(width, height);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel.startGame();
 	}
-	
+
 	public static void main(String[] args) {
 		LeagueInvaders game = new LeagueInvaders();
 	}
